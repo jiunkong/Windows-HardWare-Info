7c15ca2eedc2dfe6dba481139d2b6a4b6d35619e
Bukgeuk_ <BukgeukTV@gmail.com>
2020-02-23 12:29:16 +0900

    Example 코드 수정

--- Example/main.cpp
+++ Example/main.cpp
@@ -18,25 +18,25 @@ int main()
 	//Processor
 	WindowsHWInfo::ProcessorInfo CPUInfo;
 	CPUInfo.GetProcessorInfo();
-	 std::wstring str = (L"CPU : " + CPUInfo.ProcessorName + L", " + std::to_wstring(CPUInfo.NumberOfThread) + L" Thread");
-	 std::wcout << str << std::endl;
+	std::wstring str = (L"CPU : " + CPUInfo.ProcessorName + L", " + std::to_wstring(CPUInfo.NumberOfThread) + L" Thread");
+	std::wcout << str << std::endl;
 
-	 //Memory
-	 WindowsHWInfo::MemoryStatus MemStatus;
-	 MemStatus.IsInterger = false;
-	 MemStatus.ReturnUnit = WindowsHWInfo::MemoryUnit::GigaByte;
-	 MemStatus.GetMemoryInfo();
-	 std::cout << "Total Physics Memory : " << MemStatus.TotalPhysicsMemory << "GB" << std::endl;
-	 std::cout << "Available Physics Memory : " << MemStatus.AvailPhysicsMemory << "GB" << std::endl;
-	 std::cout << "Total Virtual Memory : " << MemStatus.TotalVirtualMemory << "GB" << std::endl;
-	 std::cout << "Available Virtual Memory : " << MemStatus.AvailVirtualMemory << "GB" << std::endl;
-	 std::cout << "Memory Usage : " << MemStatus.MemoryUtilization << "%" << std::endl;
+	//Memory
+	WindowsHWInfo::MemoryStatus MemStatus;
+	MemStatus.IsInterger = false;
+	MemStatus.ReturnUnit = WindowsHWInfo::MemoryUnit::GigaByte;
+	MemStatus.GetMemoryInfo();
+	std::cout << "Total Physics Memory : " << MemStatus.TotalPhysicsMemory << "GB" << std::endl;
+	std::cout << "Available Physics Memory : " << MemStatus.AvailPhysicsMemory << "GB" << std::endl;
+	std::cout << "Total Virtual Memory : " << MemStatus.TotalVirtualMemory << "GB" << std::endl;
+	std::cout << "Available Virtual Memory : " << MemStatus.AvailVirtualMemory << "GB" << std::endl;
+	std::cout << "Memory Usage : " << MemStatus.MemoryUtilization << "%" << std::endl;
 
-	 //OS
-	 WindowsHWInfo::OperatingSystemInfo OSInfo;
-	 OSInfo.GetOSInfo();
-	 std::wcout << OSInfo.WindowsVersion << std::endl;
-	 std::cout << "Windows Bit : " << (OSInfo.WindowsBit == WindowsHWInfo::OSbit::_32bit ? "32bit" : "64bit") << std::endl;
+	//OS
+	WindowsHWInfo::OperatingSystemInfo OSInfo;
+	OSInfo.GetOSInfo();
+	std::wcout << OSInfo.WindowsVersion << std::endl;
+	std::cout << "Windows Bit : " << (OSInfo.WindowsBit == WindowsHWInfo::OSbit::_32bit ? "32bit" : "64bit") << std::endl;
 
-	 return 0;
+	return 0;
 }
